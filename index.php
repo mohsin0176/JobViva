b41f3570f90a2b1784f64740f249b2c32da3436d
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-16 10:32:15 -0700

    Task assign done

--- index.php
+++ index.php
@@ -1,243 +1,119 @@
-
 <?php
-// used to connect to the database
-$host = "localhost";
-$db_name = "todo";
-$username = "root";
-$password = "";
-  
-try {
-    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
-} 
-// show error
-catch(PDOException $exception){
-    echo "Connection error: " . $exception->getMessage();
-}
+
+
+$databaseHost = 'localhost';
+$databaseName = 'todo';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
 ?>
 
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<!DOCTYPE HTML>
-<html>
-<head>
-    <title>Single Page To Do List</title>
-      
-    <!-- Latest compiled and minified Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
-    <style type="text/css">
-        * {
-    box-sizing: border-box;
-    font-family: -apple-system, BlinkMacSystemFont, "segoe ui", roboto, oxygen, ubuntu, cantarell, "fira sans", "droid sans", "helvetica neue", Arial, sans-serif;
-    font-size: 16px;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-}
-body {
-    background-color: #FFFFFF;
-    margin: 0;
-}
-.navtop {
-    background-color: #3f69a8;
-    height: 60px;
-    width: 100%;
-    border: 0;
-}
-.navtop div {
-    display: flex;
-    margin: 0 auto;
-    width: 1000px;
-    height: 100%;
-}
-.navtop div h1, .navtop div a {
-    display: inline-flex;
-    align-items: center;
-}
-.navtop div h1 {
-    flex: 1;
-    font-size: 24px;
-    padding: 0;
-    margin: 0;
-    color: #ecf0f6;
-    font-weight: normal;
-}
-.navtop div a {
-    padding: 0 20px;
-    text-decoration: none;
-    color: #c5d2e5;
-    font-weight: bold;
-}
-.navtop div a i {
-    padding: 2px 8px 0 0;
-}
-.navtop div a:hover {
-    color: #ecf0f6;
-}
-.content {
-    width: 1000px;
-    margin: 0 auto;
-}
-.content h2 {
-    margin: 0;
-    padding: 25px 0;
-    font-size: 22px;
-    border-bottom: 1px solid #ebebeb;
-    color: #666666;
-}
-.read .create-contact {
-    display: inline-block;
-    text-decoration: none;
-    background-color: #38b673;
-    font-weight: bold;
-    font-size: 14px;
-    color: #FFFFFF;
-    padding: 10px 15px;
-    margin: 15px 0;
-}
-.read .create-contact:hover {
-    background-color: #32a367;
-}
-.read .pagination {
-    display: flex;
-    justify-content: flex-end;
-}
-.read .pagination a {
-    display: inline-block;
-    text-decoration: none;
-    background-color: #a5a7a9;
-    font-weight: bold;
-    color: #FFFFFF;
-    padding: 5px 10px;
-    margin: 15px 0 15px 5px;
-}
-.read .pagination a:hover {
-    background-color: #999b9d;
-}
-.read table {
-    width: 100%;
-    padding-top: 30px;
-    border-collapse: collapse;
-}
-.read table thead {
-    background-color: #ebeef1;
-    border-bottom: 1px solid #d3dae0;
-}
-.read table thead td {
-    padding: 10px;
-    font-weight: bold;
-    color: #767779;
-    font-size: 14px;
-}
-.read table tbody tr {
-    border-bottom: 1px solid #d3dae0;
-}
-.read table tbody tr:nth-child(even) {
-    background-color: #fbfcfc;
-}
-.read table tbody tr:hover {
-    background-color: #376ab7;
-}
-.read table tbody tr:hover td {
-    color: #FFFFFF;
-}
-.read table tbody tr:hover td:nth-child(1) {
-    color: #FFFFFF;
-}
-.read table tbody tr td {
-    padding: 10px;
-}
-.read table tbody tr td:nth-child(1) {
-    color: #a5a7a9;
-}
-.read table tbody tr td.actions {
-    padding: 8px;
-    text-align: right;
-}
-.read table tbody tr td.actions .edit, .read table tbody tr td.actions .trash {
-    display: inline-flex;
-    text-align: right;
-    text-decoration: none;
-    color: #FFFFFF;
-    padding: 10px 12px;
-}
-.read table tbody tr td.actions .trash {
-    background-color: #b73737;
-}
-.read table tbody tr td.actions .trash:hover {
-    background-color: #a33131;
-}
-.read table tbody tr td.actions .edit {
-    background-color: #37afb7;
-}
-.read table tbody tr td.actions .edit:hover {
-    background-color: #319ca3;
-}
-.update form {
-    padding: 15px 0;
-    display: flex;
-    flex-flow: wrap;
-}
-.update form label {
-    display: inline-flex;
-    width: 400px;
-    padding: 10px 0;
-    margin-right: 25px;
-}
-.update form input {
-    padding: 10px;
-    width: 400px;
-    margin-right: 25px;
-    margin-bottom: 15px;
-    border: 1px solid #cccccc;
-}
-.update form input[type="submit"] {
-    display: block;
-    background-color: #38b673;
-    border: 0;
-    font-weight: bold;
-    font-size: 14px;
-    color: #FFFFFF;
-    cursor: pointer;
-    width: 200px;
-    margin-top: 15px;
-}
-.update form input[type="submit"]:hover {
-    background-color: #32a367;
-}
-.delete .yesno {
-    display: flex;
-}
-.delete .yesno a {
-    display: inline-block;
-    text-decoration: none;
-    background-color: #38b673;
-    font-weight: bold;
-    color: #FFFFFF;
-    padding: 10px 15px;
-    margin: 15px 10px 15px 0;
-}
-.delete .yesno a:hover {
-    background-color: #32a367;
-}
-    </style>
-          
-</head>
-<body>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <title>Single Page To Do Application</title>
+  </head>
+  <body>
+    <?php
+
+if(isset($_POST['Submit'])) {
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $asby = $_POST['asby'];
+    $subto = $_POST['subto'];
+    $date = $_POST['date'];
+    $time = $_POST['time'];
+    $result = mysqli_query($mysqli, "INSERT INTO task(id,name,asby,subto,date,time) VALUES('$id','$name','$asby','$subto','$date','$time')");   
+  }
+  ?>
   
-    <!-- container -->
     <div class="container">
-   
-        <div class="page-header">
-            <h1>Create Product</h1>
-        </div>
-      
-    <!-- html form to create product will be here -->
-          
-    </div> <!-- end .container -->
-      
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-   
-<!-- Latest compiled and minified Bootstrap JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  
-</body>
+      <div class="jumbotron">
+     <div class="text-center"> 
+      <h6>Task/Appointment Details</h6>
+    </div>
+    <div class="container">
+      <div class="jumbotron">
+        <form action="" method="post" name="form1">
+    <table width="25%" border="0">
+      <tr>
+        <td>ID</td>
+        <td><input type="text" name="id"></td>
+      </tr>
+      <tr>
+        <td>Task Name</td>
+        <td><input type="text" name="name"></td>
+      </tr>
+      <tr>
+        <td>Assign By</td>
+        <td><input type="text" name="asby"></td>
+      </tr>
+       <tr>
+        <td>Submit To</td>
+        <td><input type="text" name="subto"></td>
+      </tr>
+       <tr>
+        <td>Date</td>
+        <td><input type="text" name="date"></td>
+      </tr>
+       <tr>
+        <td>Time</td>
+        <td><input type="text" name="time"></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td><input type="submit" name="Submit" value="Create Task"></td>
+      </tr>
+    </table>
+  </form>
+      </div>
+    </div>
+    <br>
+ <table class="w3-table-all">
+
+    <tr>
+        <th>ID</th> <th>TaskName</th> <th>Assign By</th> <th>Submit To</th><th>Date</th><th>Time</th><th>Edit</th><th>Delete</th>
+    </tr>
+    <?php
+    $result = mysqli_query($mysqli, "SELECT * FROM task ORDER BY id ASC");
+    while($user_data = mysqli_fetch_array($result)) {
+        echo "<tr>";
+        echo "<td>".$user_data['id']."</td>";
+        echo "<td>".$user_data['name']."</td>";
+        echo "<td>".$user_data['asby']."</td>";
+        echo "<td>".$user_data['subto']."</td>";
+        echo "<td>".$user_data['date']."</td>";
+        echo "<td>".$user_data['time']."</td>";
+        echo "<td>Edit</td>";
+        echo "<td>Delete</td>";
+        
+    }
+    ?>
+    </table>
+
+ 
+
+</div>
+</div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+    -->
+  </body>
 </html>
