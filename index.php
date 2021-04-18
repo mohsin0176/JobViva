b5165bb5a33fd4285c67ac1ac0d49beb4e332a7a
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-18 04:53:14 -0700

    To Do List about Done

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
 
-
 $databaseHost = 'localhost';
 $databaseName = 'todo';
 $databaseUsername = 'root';
 $databasePassword = '';
-
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+$mysqli = mysqli_connect($databaseHost,$databaseUsername,$databasePassword,$databaseName);
+ 
 
 ?>
 
@@ -16,7 +15,15 @@ $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $d
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+     <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -25,7 +32,8 @@ $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $d
   <body>
     <?php
 
-if(isset($_POST['Submit'])) {
+if(isset($_POST['Submit'])) 
+{
     $id = $_POST['id'];
     $name = $_POST['name'];
     $asby = $_POST['asby'];
@@ -33,7 +41,104 @@ if(isset($_POST['Submit'])) {
     $date = $_POST['date'];
     $time = $_POST['time'];
     $result = mysqli_query($mysqli, "INSERT INTO task(id,name,asby,subto,date,time) VALUES('$id','$name','$asby','$subto','$date','$time')");   
-  }
+}
+if(isset($_GET['id']) && isset($_GET['del']))
+{
+$id = $_GET['id'];
+$result = mysqli_query($mysqli, "DELETE FROM task WHERE id=$id");
+header("Location:index.php");
+}
+if(isset($_GET['id']) && isset($_GET['ed']))
+{
+ 
+	$id = $_GET['id'];
+    $result = mysqli_query($mysqli, "SELECT * FROM task WHERE id=$id");
+    while($user_data = mysqli_fetch_array($result)) 
+    {
+
+	$id = $user_data['id'];
+	$name=$user_data['name'];
+	$asby=$user_data['asby'];
+	$subto=$user_data['subto'];
+	$date=$user_data['date'];
+	$time=$user_data['time'];        
+    }
+
+    if(isset($_POST['edit'])) 
+	{
+	$id = $_POST['id'];
+	$name=$_POST['name'];
+	$asby=$_POST['asby'];
+	$subto=$_POST['subto'];
+	$date=$_POST['date'];
+	$time=$_POST['time'];
+	$result = mysqli_query($mysqli, "UPDATE task SET id='$id',name='$name',asby='$asby',subto='$subto',date='$date',time='$time'; WHERE id=$id");
+	header("Location: index.php");
+	} 
+  
+
+?>	
+	<div class="container">
+    <div class="jumbotron">
+  <!-- Trigger the modal with a button -->
+ <button type="button" class="btn btn-default btn-sm" data-toggle="modal" data-target="#myModal" style="font-size: 15px;font-weight: 15px;">EDIT TASK :<?php echo "TaSk iD iS :".$id; ?></button>
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+      
+      <!-- Modal content-->
+      <div class="modal-content">
+ 
+        <div class="modal-body">
+          <!-----------------edit Body ----------------->
+          <form action="" method="post" name="form1">
+    <table width="100%" border="0">
+      <tr>
+        <td>ID</td>
+        <td><input type="text" name="id"value=<?php echo $id;?>></td>
+      </tr>
+      <tr>
+        <td>Task Name</td>
+        <td><input type="text" name="name" value=<?php echo $name;?>></td>
+      </tr>
+      <tr>
+        <td>Assign By</td>
+        <td><input type="text" name="asby"value=<?php echo $asby;?>></td>
+      </tr>
+       <tr>
+        <td>Submit To</td>
+        <td><input type="text" name="subto"value=<?php echo $subto;?>></td>
+      </tr>
+       <tr>
+        <td>Date(Y/M/D)</td>
+        <td><input type="text" name="date"value=<?php echo $date;?>></td>
+      </tr>
+       <tr>
+        <td>Time(H/M/S)</td>
+        <td><input type="text" name="time"value=<?php echo $time;?>></td>
+      </tr>
+
+      <tr>
+        <td></td>
+        <td><input type="submit" name="edit" value="Done"></td>
+      </tr>
+    </table>
+  </form>
+          <!----------------  end of edit body    ------------->
+        </div>
+        <div class="modal-footer">
+         <button type="button" class="btn btn-default" data-dismiss="modal" onClick="parent.location='index.php'">Quit Edit</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+  </div>
+</div>
+<?php
+
+}
+ 
   ?>
 
     <div class="container">
@@ -44,7 +149,7 @@ if(isset($_POST['Submit'])) {
     <div class="container">
       <div class="jumbotron">
         <form action="" method="post" name="form1">
-    <table width="25%" border="0">
+    <table width="100%" border="0">
       <tr>
         <td>ID</td>
         <td><input type="text" name="id"></td>
@@ -62,11 +167,11 @@ if(isset($_POST['Submit'])) {
         <td><input type="text" name="subto"></td>
       </tr>
        <tr>
-        <td>Date</td>
+        <td>Date(Y/M/D)</td>
         <td><input type="text" name="date"></td>
       </tr>
        <tr>
-        <td>Time</td>
+        <td>Time(H/M/S)</td>
         <td><input type="text" name="time"></td>
       </tr>
       <tr>
@@ -81,11 +186,12 @@ if(isset($_POST['Submit'])) {
  <table class="w3-table-all">
 
     <tr>
-        <th>ID</th> <th>TaskName</th> <th>Assign By</th> <th>Submit To</th><th>Date</th><th>Time</th><th>Edit</th><th>Delete</th>
+        <th>ID</th> <th>TaskName</th> <th>Assign By</th> <th>Submit To</th><th>Date(Y/M/D)</th><th>Time(H/M/S)</th><th>Edit</th><th>Delete</th>
     </tr>
     <?php
     $result = mysqli_query($mysqli, "SELECT * FROM task ORDER BY id ASC");
-    while($user_data = mysqli_fetch_array($result)) {
+    while($user_data = mysqli_fetch_array($result)) 
+    {
         echo "<tr>";
         echo "<td>".$user_data['id']."</td>";
         echo "<td>".$user_data['name']."</td>";
@@ -93,7 +199,8 @@ if(isset($_POST['Submit'])) {
         echo "<td>".$user_data['subto']."</td>";
         echo "<td>".$user_data['date']."</td>";
         echo "<td>".$user_data['time']."</td>";
-        echo "<td><a href='index.php?id=$user_data[id]'>Edit</a></td>"; echo "<td><a href='index.php?id=$user_data[id]'>Delete</a></td>"; 
+        echo "<td><a href='index.php?id=$user_data[id]& ed=edit'>Edit</a></td>";
+        echo "<td><a href='index.php?id=$user_data[id]& del=delete'>Delete</a></td>"; 
         
     }
     ?>
@@ -114,5 +221,6 @@ if(isset($_POST['Submit'])) {
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
     -->
+
   </body>
 </html>
